0fb3e15c6a91856d1d34f8b6af167d74a4cd9b31
ceyed <saeed.sarmad.976@gmail.com>
2022-01-10 19:54:37 +0330

    Added all servers

--- config.py
+++ config.py
@@ -3,5 +3,13 @@
 SERVER_PIN = "zgcHKBB+Owuaq4W+zBL1prdZBLDnn7mPrRryfXvea2A="
 SERVERS = {
     "Netherlands1" : "146.0.75.211",
-    "Netherlands2" : "146.0.73.41"
+    "Netherlands2" : "146.0.73.41",
+    "Finland1" : "95.216.103.115",
+    "Finland2" : "95.216.103.116",
+    "Finland3" : "95.216.103.117",
+    "Switzerland1" : "89.37.173.103",
+    "Switzerland2" : "89.37.173.104",
+    "England" : "87.236.212.84",
+    "Canada" : "144.217.230.195",
+    "France" : "176.31.83.89",
 }
